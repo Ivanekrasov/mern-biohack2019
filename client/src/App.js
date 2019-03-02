7ae03b13878750e840b1f58c35bc845328f411ca
Ivan Nekrasov <ivan_nekrasov@epam.com>
2019-03-02 05:55:30 +0300

    initial

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Navbar from './Components/Layout/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Footer from './Components/Layout/Footer';
+// import Home from './Components/Layout/Home';
+// import Social from './Components/social/Social';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.connecToServer = this.connecToServer.bind(this);
+  }
+  connecToServer() {
+    fetch('/');
+  }
+
+  componentDidMount() {
+    this.connecToServer();
+  }
+  render() {
+    return (
+      <Router>
+      <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      </div>
+      </Router>
+    );
+  }
+}
+export default App;
